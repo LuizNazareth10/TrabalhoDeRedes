16e6333cb14740c014193c14079f28dd0092905f
Luiz Nazareth <luizgustavonazareth4@gmail.com>
2025-02-16 10:00:53 -0300

    Corrigindo algumas questões

--- client.py
+++ client.py
@@ -2,6 +2,8 @@ import socket
 
 def create_client():
     c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ##TCP (SOCK_STREAM) → Comunicação confiável, baseada em conexão.
+    ##UDP (SOCK_DGRAM) → Comunicação não confiável, baseada em datagramas.
 
     for i in range(10):
         message = f'Hello, server! Message {i}'
