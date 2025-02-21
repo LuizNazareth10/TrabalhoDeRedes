a1886af7824d0e2a2840d93bec08b59be8b5ef29
Luiz Nazareth <luizgustavonazareth4@gmail.com>
2025-02-21 20:05:55 -0300

    Alterando a infraestrutura

--- client.py
+++ client.py
@@ -1,61 +1,63 @@
 import socket
+import random
 import time
 
 def create_client():
     HOST = "127.0.0.1" # Endereço do servidor (localhost)
     PORT = 8080 # Porta do servidor
     BUFFER_SIZE = 1024 # Tamanho do buffer para receber dados
+    TOTAL_PACKETS = 10000 # Número total de pacotes a serem enviados
 
     # Create UDP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Cria um socket UDP (socket.AF_INET, socket.SOCK_DGRAM), pois UDP não estabelece conexão prévia como TCP.
     server_addr = (HOST, PORT)
 
-    seq_num = 1  # seq_num representa o número do pacote a ser enviado.
+    seq_num = 0  # seq_num representa o número do pacote a ser enviado.
     cwnd = 1  # (Congestion Window) controla quantos pacotes podem ser enviados de uma vez.
-    ssthresh = 8  # (Slow Start Threshold) define o limite entre crescimento exponencial e linear.
-    last_ack = 0  # armazena o último número de sequência reconhecido pelo servidor.
+    ssthresh = 16  # (Slow Start Threshold) define o limite entre crescimento exponencial e linear.
+    # last_ack = 0  # armazena o último número de sequência reconhecido pelo servidor.
+    pac_env_not_confirm = 0  # Contador de pacotes enviados e nao confirmados.
 
 
-    while seq_num <= 20:  # Enviar até 20 pacotes
-        acks_received = 0  # Contador de ACKs recebidos
-        
-        for _ in range(cwnd):  # Send up to cwnd packets
-            if seq_num <= 20:
-                print(f"📤 Sending packet {seq_num}")
-                client_socket.sendto(str(seq_num).encode(), server_addr)#O número do pacote é convertido para string, codificado (.encode()) e enviado via UDP.
-                seq_num += 1
+    while seq_num <= TOTAL_PACKETS:  # Enviar até 10000 pacotes
+        if pac_env_not_confirm < cwnd:  # Envia pacotes enquanto o número de pacotes enviados e não confirmados for menor que cwnd.
+            msg = f"{seq_num}|Dados_{random.randint(1000, 9999)}"  # Cria uma mensagem com o número do pacote e dados aleatórios.
+            client_socket.sendto(msg.encode(), server_addr)  # Envia a mensagem para o servidor.
+            pac_env_not_confirm += 1  # Incrementa o contador de pacotes enviados e não confirmados.
+            print(f"Pacote {seq_num} enviado")
+            seq_num += 1  # Incrementa o número do pacote a ser enviado.
+            
+        try:
+            ack_data, _ = client_socket.recvfrom(BUFFER_SIZE) # Recebe resposta do servidor
+            decoded_msg = ack_data.decode()
+            print(f"Mensagem recebida do servidor: {decoded_msg}")  # Debugging
+            
+            parts = decoded_msg.split("|")
+            if len(parts) != 3:
+                print(f"Erro: Mensagem inesperada recebida: {decoded_msg}")
+            else:
+                ack, ack_num, rwnd = parts
+                ack_num = int(ack_num)
+                rwnd = int(rwnd)
+                if ack == "ACK":
+                    print(f"Server confirmou até {ack_num}")
 
-        
-        while acks_received < cwnd: #Se acks_received for menor que cwnd, ainda há pacotes aguardando ACKs.
-            try:
-                new_cwnd = cwnd
-                client_socket.settimeout(3)  # Timeout de 3 segundos. Se o cliente não receber ACKs dentro desse tempo, assume que os pacotes foram perdidos.
-                ack_data, _ = client_socket.recvfrom(BUFFER_SIZE) #recvfrom(BUFFER_SIZE) recebe a resposta do servidor.
-                ack_msg = ack_data.decode()
-                last_ack = int(ack_msg.split()[1])#Atualiza last_ack com o número do último pacote confirmado.
-                print(f"Server confirmou até {last_ack}")
-
-                acks_received += 1
-                print("acks_received: ", acks_received)
-                print("cwmd: ", cwnd)
-
+                pac_env_not_confirm -= (ack_num - (seq_num - pac_env_not_confirm))+1
+                
                 if cwnd < ssthresh:
                     cwnd *= 2  # Se cwnd ainda for menor que ssthresh, o crescimento é exponencial (multiplica por 2).
                 else:
-                    cwnd += 1  # Se cwnd >= ssthresh, o crescimento é linear (soma 1).
+                    cwnd += 1
+                    
+                cwnd = min(cwnd, rwnd)  # Limita cwnd ao tamanho da janela de recebimento.
+            
 
-                if acks_received == new_cwnd:
-                    break
-
-
-
-            except socket.timeout: #Se o cliente não recebe um ACK em 3 segundos, ocorre um timeout.
-                print("Timeout: No response from server. Ajustando cwnd e ssthresh...")
-                #Isso significa perda de pacotes, então ajustamos ssthresh e cwnd para simular TCP
-                ssthresh = max(2, cwnd // 2)  # Reduz ssthresh pela metade
-                cwnd = 1  # Volta para Slow Start
-                seq_num = last_ack + 1  # Reenvia pacotes perdidos
-                break
+        
+        except socket.timeout:  # Se o tempo de espera expirar, reenvie os pacotes.
+            print("Tempo de espera expirou, reenviando pacotes.")
+            ssthresh = max(cwnd // 2, 1)
+            cwnd = 1
+            pac_env_not_confirm = 0
 
 
 create_client()
--- server.py
+++ server.py
@@ -5,24 +5,26 @@ import random
 
 def create_server():
     # Server settings
-    HOST = "127.0.0.1" #Endereço do servidor (localhost)
-    PORT = 8080 #Porta do servidor
+    SERVER_IP = "127.0.0.1" #Endereço do servidor (localhost)
+    SERVER_PORT = 8080 #Porta do servidor
     BUFFER_SIZE = 1024 #Tamanho do buffer para receber dados
     WIN_SIZE = 3  # Tamanho da janela deslizante REVISAR A NECESSIDADE DESSE CARA
     LOSS_PROB = 0.1 # Probabilidade de perda de pacotes(10%)
+    RWND = 3  # Tamanho da janela de recebimento (controle de fluxo será aplicado aqui)
 
     # Create UDP socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Cria um socket UDP (socket.AF_INET, socket.SOCK_DGRAM), pois UDP não estabelece conexão prévia como TCP.
-    server_socket.bind((HOST, PORT)) #Associa o socket ao endereço e porta do servidor.
+    server_socket.bind((SERVER_IP, SERVER_PORT)) #Associa o socket ao endereço e porta do servidor.
 
-    print(f"Server running on {HOST}:{PORT}, Window Size = {WIN_SIZE}")
+    print(f"Server running on {SERVER_IP}:{SERVER_PORT}, Window Size = {WIN_SIZE}")
 
     received_packets = {}  #  Um dicionário que armazena pacotes recebidos
-    expected_seq = 1  # Controla qual número de sequência o servidor espera receber.
+    expected_seq = 0  # Controla qual número de sequência o servidor espera receber.
 
     while True:
         data, addr = server_socket.recvfrom(BUFFER_SIZE) # Espera receber um pacote
-        seq_num = int(data.decode())  # Converte o número do pacote recebido
+        seq_num, msg = data.decode().split("|", 1)
+        seq_num = int(seq_num) # Converte o número do pacote recebido
         #O servidor fica rodando indefinidamente, esperando pacotes.
         # recvfrom(BUFFER_SIZE) → Aguarda receber dados do cliente e retorna:
         # data → O conteúdo recebido (número do pacote).
@@ -30,25 +32,32 @@ def create_server():
 
         if random.random() < LOSS_PROB:
             print(f"Pacote {seq_num} não chegou no server!")
+            continue
             #random.random() gera um número aleatório entre 0 e 1.
             # Se esse número for menor que LOSS_PROB (0.1), significa que o pacote foi "perdido".
             # O servidor simula essa perda ignorando o pacote e não enviando ACK.
-
-        else:
-            time.sleep(0.1) #simular tempo de processamento de pacote
+        print(f"Pacote recebido {seq_num}")
 
 
-            if seq_num == expected_seq:
-                print(f"Packet {seq_num} received correctly.")
-                received_packets[seq_num] = True
+        if seq_num == expected_seq:
+            print(f"Packet {seq_num} received correctly.")
+            # received_packets[seq_num] = True
+            expected_seq += 1
+            
+            while expected_seq in received_packets:
+                print(f"Entregando pacote {expected_seq} a aplicação")
                 expected_seq += 1
-            else:
-                print(f"Packet {seq_num} out of order. Expected {expected_seq}")
-
-            # Send ACK for the last correctly received sequence number
-            print(f"📥 Sending ACK {expected_seq-1}")
-            ack_msg = f"ACK {expected_seq-1}"
-            server_socket.sendto(ack_msg.encode(), addr)
+            
+        else:
+            print(f"Packet {seq_num} out of order. Expected {expected_seq}")
+            del received_packets[seq_num]
+            # received_packets[seq_num] = False
+            expected_seq += 1
+            
+        # Send ACK for the last correctly received sequence number
+        print(f"📥 Sending ACK {expected_seq-1}")
+        ack_msg = f"ACK|{expected_seq-1}|{RWND}"
+        server_socket.sendto(ack_msg.encode(), addr)
             
 
 create_server()
